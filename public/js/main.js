5fe2ef230a5380bbe662342809794ba3aea0b6ef
Nuria <c.odena.nuria@gmail.com>
2020-04-21 16:11:47 +0200

    displaying dynamic data

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
 
 const socket = io();
 
@@ -6,7 +7,12 @@ const socket = io();
 socket.on('message', message => {
     console.log(message);
     outputMessage(message);
+
+    // Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 });
+
+
 
 // Message submit
 chatForm.addEventListener('submit', (e) => {
@@ -17,6 +23,10 @@ const msg = e.target.elements.msg.value;
 
 // Emit message to server
 socket.emit('chatMessage', msg)
+
+// Clear input
+e.target.elements.msg.value = '';
+e.target.elements.msg.focus();
 });
 
 // Output message to DOM
@@ -24,9 +34,9 @@ socket.emit('chatMessage', msg)
 function outputMessage(message){
     const div = document.createElement('div');
     div.classList.add('message');
-    div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+    div.innerHTML = `<p class="meta">${message.username} <span>~${message.time}</span></p>
     <p class="text">
-        ${message}
+        ${message.text}
     </p>`;
     document.querySelector('.chat-messages').appendChild(div);
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,22 +11,25 @@ const io = socketio(server);
 // Sset static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'Admin Bot';
+
 // Run when client connects
 io.on('connection', socket => {
-    console.log('New WS Connection...');
-    socket.emit('message', 'Welcome to Chat App!');
+
+    // Welcome current user
+    socket.emit('message', formatMessage(botName, 'Welcome to Chat App!'));
 
     // Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has just joined the chat');
+    socket.broadcast.emit('message', formatMessage(botName,'A user has just joined the chat'));
 
     // Runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has just left the chat');
+        io.emit('message', formatMessage(botName,'A user has just left the chat'));
     });
 
     // Listen for chat message
     socket.on('chatMessage', msg => {
-        io.emit('message', msg);
+        io.emit('message', formatMessage('USER', msg));
     });
 });
 const PORT = 3000 || process.env.PORT;
