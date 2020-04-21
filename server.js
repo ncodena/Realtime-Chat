e6466dc77172d56e9d3a9a39eb20c8ec40b4cccf
Nuria <c.odena.nuria@gmail.com>
2020-04-21 18:06:26 +0200

    displaying specific chatroom and user

--- server.js
+++ server.js
@@ -33,7 +33,8 @@ io.on('connection', socket => {
 
     // Listen for chat message
     socket.on('chatMessage', msg => {
-        io.emit('message', formatMessage('USER', msg));
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
     });
 
     // Runs when client disconnects
