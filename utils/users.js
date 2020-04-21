f684d5d337db1a0904d5e64beb02dd3baca7e5f7
Nuria <c.odena.nuria@gmail.com>
2020-04-21 18:44:36 +0200

    displaying leaving chat activity

--- utils/users.js
+++ utils/users.js
@@ -18,7 +18,23 @@ function getCurrentUser(id) {
     return users.find(user => user.id === id);
 }
 
+// User leaves chat
+
+function userLeave(id) {
+    const index = users.findIndex(user => user.id == id);
+
+    if(index !== -1) {
+        return users.splice(index, 1)[0];
+    }
+}
+
+function getRoomUsers(room) {
+    return users.filter(user => user.room === room)
+}
+
 module.exports = {
     userJoin,
-    getCurrentUser
+    getCurrentUser,
+    userLeave,
+    getRoomUsers
 }
